1e5cc5d709c2277379b7dd9e0aa67f469e0e740e
Dhivya-56 <dhivyag0506@gmail.com>
2024-10-02 19:54:01 +0530

    Js started

---
+++ Feedback form/Javascript/Variables/variables.js
@@ -0,0 +1,26 @@
+//using var
+// var firstName="Dhivya"
+// console.log(firstName)
+
+var Age=30; //declared
+Age=20          /// reassigned
+console.log(Age)
+
+//using let
+// let firstName="Dhivya"
+// console.log(firstName)
+
+let myScore=10; //declared
+myScore=50       //reassign
+
+console.log(myScore)
+
+
+//using const
+// const firstName="Dhivya"
+// console.log(firstName)
+
+const grade='A' // declare
+grade='B'     //if reassign ,gives TypeError
+console.log(grade)
+
